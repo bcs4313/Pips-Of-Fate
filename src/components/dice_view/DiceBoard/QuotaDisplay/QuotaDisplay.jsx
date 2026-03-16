1042e9f47f841201b786e025a11064851fe74804
bcs4313 <bcs4313@g.rit.edu>
2026-03-16 04:14:12 -0400

    Improved UI aesthetics of quota bar.

--- src/components/dice_view/DiceBoard/QuotaDisplay/QuotaDisplay.jsx
+++ src/components/dice_view/DiceBoard/QuotaDisplay/QuotaDisplay.jsx
@@ -6,11 +6,11 @@ export default function QuotaDisplay({totalDiceScore, quotaRequired, rollsLeft})
     // scored out of 100 for the progress bar
     const quotaProgressAmount = Math.min((totalDiceScore / quotaRequired) * 100, 100)
     return (
-        <div className="quota-parent">
+        <div className="quota-parent @container">
             <div className="quota-row">
-                <strong className="quota-strong text-white">Score: {totalDiceScore} </strong>
-                <strong className="quota-strong text-white">Required Score: {quotaRequired} </strong>
-                <strong className="quota-strong text-white">Rolls Remaining: {rollsLeft}</strong>
+                <strong className="quota-strong text-white text-[1.5cqi]">Score: {totalDiceScore} </strong>
+                <strong className="quota-strong text-white text-[1.5cqi]">Required Score: {quotaRequired} </strong>
+                <strong className="quota-strong text-white text-[1.5cqi]">Rolls Remaining: {rollsLeft}</strong>
             </div>
             <Progress className="quota-progress" animated="true" value={quotaProgressAmount}/>
         </div>
