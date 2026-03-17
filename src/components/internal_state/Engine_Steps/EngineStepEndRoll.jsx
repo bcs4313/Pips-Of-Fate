6fb5cf8544329612262530d99055747b43f1c24f
Cody Smith <70149089+bcs4313@users.noreply.github.com>
2026-03-17 05:34:37 -0400

    Delete src/components/internal_state/Engine_Steps directory

--- src/components/internal_state/Engine_Steps/EngineStepEndRoll.jsx
+++
@@ -1,7 +0,0 @@
-import { useInventory } from "../../items/InventoryContextProvider"
-
-export default function EngineStepEndRoll(engineState, inventoryInterface, hooks) {
-    // trigger items under this step
-    let postItemEngineState = inventoryInterface.forwardStep("END_ROLL", engineState, inventoryInterface, hooks)
-    return postItemEngineState
-}
--- src/components/internal_state/Engine_Steps/EngineStepPreRollResults.jsx
+++
@@ -1,7 +0,0 @@
-import { useInventory } from "../../items/InventoryContextProvider"
-
-export default function EngineStepPreRollResults(engineState, inventoryInterface, hooks) {
-    // trigger items under this step
-    let postItemEngineState = inventoryInterface.forwardStep("PRE_ROLL_RESULT", engineState, inventoryInterface, hooks)
-    return postItemEngineState
-}
